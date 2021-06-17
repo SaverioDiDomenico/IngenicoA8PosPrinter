9ecbdd4a846989df97ec07e59695f6c7733c3b91
SaverioDiDomenico <34535692+SaverioDiDomenico@users.noreply.github.com>
2021-06-17 23:04:16 +0200

    Add files via upload

---
+++ src/android/SpinnerItem.java
@@ -0,0 +1,33 @@
+package it.dynamicid;
+
+/**
+ * ä¸‹æ‹‰æ¡†æ¯�ä¸ªé€‰é¡¹çš„å†…å®¹
+ */
+public class SpinnerItem {
+    int value;
+    String name;
+    String strValue;
+
+    public SpinnerItem (int value, String name) {
+        this.value = value;
+        this.name = name;
+    }
+
+    public SpinnerItem (String value, String name) {
+        this.strValue = value;
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getStringValue() {
+        return strValue;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}

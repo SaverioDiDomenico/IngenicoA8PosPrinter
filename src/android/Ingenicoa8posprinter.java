cb845790882247bff6ae46fc3fe435fbfca3d10c
SaverioDiDomenico <34535692+SaverioDiDomenico@users.noreply.github.com>
2021-06-17 17:48:38 +0200

    Update Ingenicoa8posprinter.java

--- src/android/Ingenicoa8posprinter.java
+++ src/android/Ingenicoa8posprinter.java
@@ -2,6 +2,11 @@ package android;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+import android.content.Context;
+import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.LinkedList;
